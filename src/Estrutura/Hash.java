e7791ccd4f405ef53587407e793db48b2ef01fc6
luanavnn-ctrl <luanavidalnn@gmail.com>
2022-12-06 16:27:51 -0300

    update documentation

--- src/Estrutura/Hash.java
+++ src/Estrutura/Hash.java
@@ -1,33 +1,32 @@
 package Estrutura;
 
-public class Hash {             
-    int operador;   
-    Lista[] vetor;                  
+public class Hash {                            //classe hash              
+    int operador;                              //atributo operador 
+    Lista[] vetor;                             //vetor de listas 
 
-    public Hash(int operador){
-        this.operador = operador;
-        vetor = new Lista[operador];
-        for (int i = 0; i < operador; i++) {
-            vetor[i] = new Lista();
+    public Hash(int operador){                 //contrutor iniciando com um operador 
+        this.operador = operador;              //inicializa o operador 
+        vetor = new Lista[operador];           //inicializa o vetor de Listas 
+        for (int i = 0; i < operador; i++) {   //para cada posicao no vetor 
+            vetor[i] = new Lista();            //inicializa a Lista daquela posicao do vetor 
         }
     }
 
-    public void inserir(Contato contato) {
-        int chave = contato.id % operador;
-        vetor[chave].inserir(contato);        
+    public void inserir(Contato contato) {    //metodo inserir um contato           
+        int chave = contato.id % operador;    //posicao = resto do id / operador
+        vetor[chave].inserir(contato);        //insere o contato naquela lista
     }
 
-    public Contato buscar(int id) {
-        return vetor[id % operador].buscar(id);
+    public Contato buscar(int id) {             //metodo buscar pelo id
+        return vetor[id % operador].buscar(id); //busca apenas na lista especifica
     }
-
     
-    public String toString() {
-        String out = "";
-        for(int i = 0; i < operador; i++) {
-            out += "" + i + ": ";
-            out += vetor[i % operador] + "\n";
+    public String toString() {                  //sobrescreve o metodo toString              
+        String out = "";                        //cria uma string de saida  
+        for(int i = 0; i < operador; i++) {     //para cada posicao no vetor de Listas
+            out += "" + i + ": ";               //adiciona uma string representando a Lista
+            out += vetor[i % operador] + "\n";  
         }
-        return out;
+        return out;                             //retorna a string
     }
 }
