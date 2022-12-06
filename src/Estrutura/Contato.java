b23e6923a92a841fd51f2577b90adc6e99b72a9c
luanavnn-ctrl <luanavidalnn@gmail.com>
2022-12-06 18:09:13 -0300

    upload documentation

--- src/Estrutura/Contato.java
+++ src/Estrutura/Contato.java
@@ -1,20 +1,20 @@
 package Estrutura;
 
-public class Contato {      //classe contato
-    public int id;          //id do contato
-    String nome;            //nome do contato
-    String aniversario;     //aniversario do contato
+public class Contato {                                              //classe contato
+    public int id;                                                  //id do contato
+    String nome;                                                    //nome do contato
+    String aniversario;                                             //aniversario do contato
 
 
-    public Contato (int id, String nome, String aniversario) {  //construtor com parametros
-        this.id = id;                                           //inicializa o atributo id   
-        this.nome = nome;                                       //inicializa o atributo nome   
-        this.aniversario = aniversario;                         //inicializa o atributo aniversario
+    public Contato (int id, String nome, String aniversario) {      //construtor com parametros
+        this.id = id;                                               //inicializa o atributo id   
+        this.nome = nome;                                           //inicializa o atributo nome   
+        this.aniversario = aniversario;                             //inicializa o atributo aniversario
     }
 
     
 
-    public String toString(){                   //sobrescreve o metodo toString
+    public String toString(){                                       //sobrescreve o metodo toString
         return "(" + id + ", " + nome + ", " + aniversario + ")";   //retorna codigo, nome e aniversario
     }
 }
--- src/Estrutura/Hash.java
+++ src/Estrutura/Hash.java
@@ -1,20 +1,20 @@
 package Estrutura;
 
-public class Hash {                            //classe hash              
-    int operador;                              //atributo operador 
-    Lista[] vetor;                             //vetor de listas 
+public class Hash {                             //classe hash              
+    int operador;                               //atributo operador 
+    Lista[] vetor;                              //vetor de listas 
 
-    public Hash(int operador){                 //contrutor iniciando com um operador 
-        this.operador = operador;              //inicializa o operador 
-        vetor = new Lista[operador];           //inicializa o vetor de Listas 
-        for (int i = 0; i < operador; i++) {   //para cada posicao no vetor 
-            vetor[i] = new Lista();            //inicializa a Lista daquela posicao do vetor 
+    public Hash(int operador){                  //contrutor iniciando com um operador 
+        this.operador = operador;               //inicializa o operador 
+        vetor = new Lista[operador];            //inicializa o vetor de Listas 
+        for (int i = 0; i < operador; i++) {    //para cada posicao no vetor 
+            vetor[i] = new Lista();             //inicializa a Lista daquela posicao do vetor 
         }
     }
 
-    public void inserir(Contato contato) {    //metodo inserir um contato           
-        int chave = contato.id % operador;    //posicao = resto do id / operador
-        vetor[chave].inserir(contato);        //insere o contato naquela lista
+    public void inserir(Contato contato) {      //metodo inserir um contato           
+        int chave = contato.id % operador;      //posicao = resto do id / operador
+        vetor[chave].inserir(contato);          //insere o contato naquela lista
     }
 
     public Contato buscar(int id) {             //metodo buscar pelo id
